25b043065fbd5838a9ead53217d0167dd946b54f
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-30 19:41:28 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/******************************************************************************\
+* Name: Jason D. F. D'Oyley
+* Date: October 30, 2022
+* Assignment CIS214 Week 4 GP - The Finally Block
+*
+* Main application (program) class.
+* In this application we will demonstrate the use of exception handling with a
+* finally block.
+\******************************************************************************/
+
+public class FinallyDemo
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
