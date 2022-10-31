a5ce5a353a167c91b5d63662999a53a36f85ef97
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-30 20:16:17 -0400

    Demo the finally block

--- Program.cs
+++ Program.cs
@@ -12,6 +12,73 @@ public class FinallyDemo
 {
     static void Main(string[] args)
     {
+        // Print a header line
+        Console.WriteLine("\nJason D'Oyley - Week 4 GP - The Finally Block\n");
 
+        try
+        {
+            ThrowException();
+        }
+        catch (Exception e)
+        {
+            // Caught the exception thrown by throw
+            Console.WriteLine(
+                "Exception handled in main. Message: " + e.Message
+            );
+        }
+
+        DoesNotThrowException();
+    }
+
+    // Demonstrate try...catch...finally
+    public static void ThrowException()
+    {
+        // Throw an exception and immediately catch it
+        try
+        {
+            Console.WriteLine("\nMethod ThrowException");
+            throw new Exception("Exception thrown in ThrowException");
+        }
+        catch
+        {
+            Console.WriteLine("Exception handled in method ThrowException");
+
+            // Re-throw the exception for further processing
+            throw;
+        }
+        finally
+        {
+            // Executes regardles of what happens in try...catch
+            Console.WriteLine("Finally executed in ThrowException");
+        }
+
+        // Any code placed here would not be reached and would generate
+        // comilation warnings because of the throw in the try block - the
+        // warning generated is an "unreachable code" warning, which will happen
+        // any time a block of code can never be executed; if you uncomment the
+        // line of code below, you'll see this demonstrated
+        // Console.WriteLine("Program generates warning with this uncommented.")
+    }
+
+    // Demonstrate finally with no exception thrown
+    public static void DoesNotThrowException()
+    {
+        // Don't throw an exception
+        try
+        {
+            Console.WriteLine("\nMethod DoesNotThrowException");
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        finally
+        {
+            // Executes regardless of what happens in try...catch
+            Console.WriteLine("Finally executed in DoesNotThrowException");
+        }
+
+        Console.WriteLine("End of method DoesNotThrowException");
     }
 }
